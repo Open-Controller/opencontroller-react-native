77dd5e5aa65c8614c5f1549c1babe4e755ca4fa6
Peter PJT <pjtsignups@gmail.com>
2020-07-28 17:49:22 -0700

    feat(splash): change splash color

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import changeNavigationBarColor from "react-native-navigation-bar-color"
 
 export default function App() {
+  changeNavigationBarColor('transparent');
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"/>
     </View>
   );
 }
