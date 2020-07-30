f76ff13d065a8e24ff5b3069458bb50d426badf4
Peter PJT <pjtsignups@gmail.com>
2020-07-30 15:39:44 -0700

    feat(ControllerDisplay): support multiple sections

--- components/ControllerDispaly.tsx
+++ components/ControllerDispaly.tsx
@@ -1,19 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Controller, Custom, Button } from 'control-lib';
+import { View } from 'react-native';
+import { Controller } from 'control-lib';
 import WidgetDisplay from './Widget/WidgetDisplay';
 
 export default function ControllerDisplay({controller}:{controller:Controller}) {
-  // console.log(JSON.stringify(controller,null,1))
-  return (
-    // <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-    //  {controller.name}
-    // </Button>
-    <WidgetDisplay widget={controller.layout[0]}/>
-  );
+  return <View>
+    {controller.layout.map(widget=>
+      <WidgetDisplay widget={widget}/>
+    )}
+  </View>
 }
-
-const styles = StyleSheet.create({
-  
-});
