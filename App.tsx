93a6df3dd5dfd3ca92f18d937f48797be8768d87
Peter PJT <pjtsignups@gmail.com>
2020-07-31 11:05:50 -0700

    refactor(sample-data): move the test data to sample-data

--- App.tsx
+++ App.tsx
@@ -3,16 +3,13 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Button, Text, Surface, useTheme } from 'react-native-paper';
 import ControllerDisplay from './components/ControllerDispaly';
-import { home } from './data/home';
+import { home } from './sample-data/home';
 import { House } from 'control-lib';
 export default function App() {
-  const theme = useTheme();
-  return (
-    <Surface style={styles.container}>
+  return <Surface style={styles.container}>
       <ControllerDisplay controller={House.fromJSON(JSON.parse(JSON.stringify(home))).rooms[0].controllers[0]}/>
       <StatusBar style="auto"/>
     </Surface>
-  );
 }
 
 const styles = StyleSheet.create({
